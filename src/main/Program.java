31803ed723f192b1befe500a15d44d4c6b6a873d
SpongeStark <yksponge@gmail.com>
2021-10-22 13:35:52 +0200

    git ignore

--- src/main/Program.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-public class Program {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello again");
-    }
-}
