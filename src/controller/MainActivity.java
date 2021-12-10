df7d9783fdd929e8328472dfce2501a287559d82
SpongeStark <yksponge@gmail.com>
2021-12-10 12:38:44 +0100

    还原

--- src/controller/MainActivity.java
+++ src/controller/MainActivity.java
@@ -1,13 +1,10 @@
 package controller;
 
 import view.MainWindow;
-import view.MyFileDialog;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.io.File;
 
 public class MainActivity {
     MainWindow window;
@@ -19,25 +16,25 @@ public class MainActivity {
 
     private void btnFind_onClick(ActionEvent e) {
 //        JOptionPane.showMessageDialog(null, "Ton message");
-//        JFileChooser fileChooser = new JFileChooser();
-//        //file name filter
-//        FileNameExtensionFilter filter = new FileNameExtensionFilter("Text file","txt");
-//        fileChooser.setFileFilter(filter);
-//        int result = fileChooser.showOpenDialog(window);
-//        if(result == JFileChooser.APPROVE_OPTION){
-//            window.txt_path.setText(fileChooser.getSelectedFile().getAbsolutePath());
-//        }
-        FileDialog fileChooser = new FileDialog(window);
+        JFileChooser fileChooser = new JFileChooser();
+        //file name filter
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Text file","txt");
+        fileChooser.setFileFilter(filter);
+        int result = fileChooser.showOpenDialog(window);
+        if(result == JFileChooser.APPROVE_OPTION){
+            window.txt_path.setText(fileChooser.getSelectedFile().getAbsolutePath());
+        }
+//        FileDialog fileChooser = new FileDialog(window);
         //file name filter
 //        FileNameExtensionFilter filter = new FileNameExtensionFilter("Text file","txt");
 //        fileChooser.setFileFilter(filter);
-        fileChooser.show();
-        fileChooser.setMode(FileDialog.LOAD);
-        String filePath = fileChooser.getDirectory();
-        String fileName = fileChooser.getFile();
-        if(filePath!=null && fileName!=null){
-            window.txt_path.setText(filePath+fileName);
-        }
+//        fileChooser.show();
+//        fileChooser.setMode(FileDialog.LOAD);
+//        String filePath = fileChooser.getDirectory();
+//        String fileName = fileChooser.getFile();
+//        if(filePath!=null && fileName!=null){
+//            window.txt_path.setText(filePath+fileName);
+//        }
 
 
 
